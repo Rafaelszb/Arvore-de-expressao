ce49f8c12116b659eba91f152f5bd269947c8ff3
Rafaelszb <rafaelsouzas153@gmail.com>
2025-07-06 11:05:03 -0300

    Salvo

--- biblioteca.h
+++ biblioteca.h
@@ -2,7 +2,7 @@
 #define BIBLIOTECA_H
 
 #include <iostream>
-#include <string>
+#include <vector>
 
 
 using namespace std;
--- main.cpp
+++ main.cpp
@@ -2,11 +2,11 @@
 
 int main() {
     Node *root = new Node();
-    montar_string("((5+5)+(5+5))*1", root);
+    montar_string("((10*3)+(40/(-2)))*2", root);
 
     // + + 5 5 + 5 5
     printPreOrder(root);
-
+cout << "Resultado: " << executar_arvore(root) << endl; // 19.8
 
     return 0;
 }
